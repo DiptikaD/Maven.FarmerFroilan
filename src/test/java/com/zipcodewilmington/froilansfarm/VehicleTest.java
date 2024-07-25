04939380f381d7feb1de7e0cf5ea24615afd949b
Will <will@Zipcoders-MacBook-Pro-9.local>
2024-07-25 17:47:53 -0400

    made more changes

--- src/test/java/com/zipcodewilmington/froilansfarm/VehicleTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/VehicleTest.java
@@ -1,4 +1,16 @@
 package com.zipcodewilmington.froilansfarm;
 
+import org.junit.Test;
+
 public class VehicleTest {
+
+ @Test
+ public void nullaryConstructorTest(){
+     Vehicle vehicle = new Vehicle;
+
+
+
+ }
+
+
 }
