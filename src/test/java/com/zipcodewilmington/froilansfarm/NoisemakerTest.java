87be3003ebe28524a1c044816446dfb9686ffae9
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-07-25 17:51:59 -0400

    added person

--- src/test/java/com/zipcodewilmington/froilansfarm/NoisemakerTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/NoisemakerTest.java
@@ -6,8 +6,14 @@ import org.junit.Test;
 public class NoisemakerTest {
 
     @Test
-    public void makeNoiseTest(){
+    public void makeNoiseHorseTest(){
         Horse horse = new Horse();
         Assert.assertEquals("Neigh", horse.makeNoise());
+    }
+
+    @Test
+    public void makeNoisePersonTest(){
+        Animal person = new Person();
+        Assert.assertEquals("Hi", person.makeNoise());
     }
 }
