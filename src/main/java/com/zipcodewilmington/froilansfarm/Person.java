5bcd6e65bfeb1bc4b26a409ca0d8436db0a125a4
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-07-25 19:06:11 -0400

    added person constructor pilot and farmer

--- src/main/java/com/zipcodewilmington/froilansfarm/Person.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Person.java
@@ -2,6 +2,15 @@ package com.zipcodewilmington.froilansfarm;
 
 public class Person extends Animal implements Rider {
 
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+
+    }
     @Override
     public String makeNoise() {
         return "Hi";
@@ -10,5 +19,13 @@ public class Person extends Animal implements Rider {
     @Override
     public void eat(Edible Edible) {
         stomach.add(Edible);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
