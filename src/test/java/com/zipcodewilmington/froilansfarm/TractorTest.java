5d1839ab7659d464bbf8498bcddee992ea02e2e9
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-07-26 14:43:10 -0400

    added harvest

--- src/test/java/com/zipcodewilmington/froilansfarm/TractorTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/TractorTest.java
@@ -13,8 +13,6 @@ public class TractorTest {
         tractor.operate();
 
         Assert.assertTrue(tractor.operate());
-
-
     }
 
     @Test
@@ -24,5 +22,15 @@ public class TractorTest {
         tractor.makeNoise();
 
         Assert.assertEquals(makeNoise,tractor.makeNoise());
+    }
+
+    @Test
+    public void harvestedTest(){
+        Tractor tractor = new Tractor();
+        EarCorn earCorn = new EarCorn();
+        Tomato tomato = new Tomato();
+        tractor.harvest(earCorn);
+        tractor.harvest(tomato);
+        Assert.assertEquals(2, tractor.harvested.size());
     }
 }
